ad9d130c6723f2829c4db8d418bce7f015dfd2f6
Pedro Koberstain <113212789+pedrokoberstain@users.noreply.github.com>
2023-03-19 16:57:34 -0300

    Add files via upload

---
+++ EstudosDIO/Classes/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+package EstudosDIO.Classes;
+
+import java.sql.SQLOutput;
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova Minerva");
+        else
+            System.out.println("Aprovado");
+    }
+}
